52fa223b356e045b7d519e9e44425e3f7228e21d
seriouslag <Landon.Gavin@gmail.com>
2019-03-12 00:27:41 -0400

    updated font, removed extra div

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,6 @@ export default class App extends Vue {
 
 <style lang="scss" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
